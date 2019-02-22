c89b7974adf762d41d505b2cc180217bbf55730c
Yinbin Zuo <singemazuo@gmail.com>
2019-02-22 12:27:08 -0400

    / ‘Demo/database-demo/app.js’

---
+++ Demo/database-demo/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const Author = require('./models/author');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index',{ title : 'Home' });
+});
+
+app.get('/author',(req,res) => {
+    res.render('author');
+});
+
+app.get('/author/all',(req,res) => {
+    Author.find({},(err,docs) => {
+        res.send(docs);
+    });
+});
+
+app.all('/author/create',(req,res) => {
+    let author = new Author({fname:req.query.fname,lname:req.query.lname});
+    author.save((err) => {
+        if(err)
+            res.send(err);
+        else
+            res.send("successed!");
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
