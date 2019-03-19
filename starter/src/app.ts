dc165d42a165d89e4e7e44bc0a140e5e88108a86
Yinbin Zuo <singemazuo@gmail.com>
2019-03-19 10:36:33 -0300

    + ‘starter/.env’ + ‘starter/.vscode/launch.json’ + ‘starter/package-lock.json’ + ‘starter/package.json’ + ‘starter/src/app.ts’ + ‘starter/src/controller/BaseController.ts’ + ‘starter/src/controller/IController.ts’ + ‘starter/src/controller/JokeController.ts’ + ‘starter/src/global.d.ts’ + ‘starter/src/middleware/sessionAuth.ts’ + ‘starter/src/models/Category.ts’ + ‘starter/src/models/Joke.ts’ + ‘starter/src/models/User.ts’ + ‘starter/src/public/js/jokes.js’ + ‘starter/src/server.ts’ + ‘starter/src/views/css/style.scss’ + ‘starter/src/views/pages/jokes.ejs’ + ‘starter/src/views/partials/head.ejs’ + ‘starter/src/views/partials/nav.ejs’ + ‘starter/tools/copyAssets.ts’ + ‘starter/tsconfig.json’ + ‘starter/tslint.json’

---
+++ starter/src/app.ts
@@ -0,0 +1,71 @@
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+
+import BaseController from "./controller/BaseController";
+import IController from "./controller/IController";
+import JokeController from "./controller/JokeController";
+
+const controllers = new Array<IController>();
+
+class App {
+  public app: express.Application;
+  public port: any;
+
+  constructor() {
+    this.app = express();
+
+    this.initlization();
+    this.initializeDatabase();
+    this.initializeMiddlewares();
+    this.initializeViewEngine();
+    this.initializeControllers();
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log( `server started at http://localhost:${ this.port }` );
+    });
+  }
+
+  private initlization() {
+    dotenv.config();
+
+    this.port = process.env.SERVER_PORT;
+
+    controllers.push(new JokeController());
+  }
+
+  private initializeDatabase() {
+    mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }).then(
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+    ).catch((err: any) => {
+        // console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+        // process.exit();
+    });
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+
+    this.app.use(express.static("src/public"));
+    // this.app.use(express.static(path.join(process.cwd(), 'public')));
+  }
+
+  private initializeViewEngine() {
+    this.app.set( "views", path.join( __dirname, "views" ) );
+    this.app.set( "view engine", "ejs" );
+  }
+
+  private initializeControllers() {
+    controllers.forEach((controller: BaseController & IController) => {
+      controller.setupRouter(this.app);
+    });
+  }
+}
+
+export default App;
